5446f72afe95a47624cbc61d5b75552a0d8229d4
abhishek96k <abhisheknalawade2711@gmail.com>
2023-08-18 14:55:23 +0530

    Chat Feature

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,7 @@
 const GOOGLE_API_KEY = "AIzaSyCtafTabusVOxYh4eo-dUEKFNkY-8uV-7c";
 
+export const OFFSET_LIVE_CHAT = 20;
+
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
